c67408477e5f9a2f232ee77117cef4c12d639535
claire <claire.kimani@student.moringaschool.com>
2025-04-12 22:16:09 +0300

    changes in the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
@@ -24,6 +31,7 @@ function App() {
     <div>
       <NavBar />
       <Home /> 
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
